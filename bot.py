d475185f962ca6964c20981bad9100c0cabf963b
Ishan Rajpal <56106026+ishanrajpal@users.noreply.github.com>
2020-08-14 18:26:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ client.remove_command('help')
 
 global name
 
+
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('With my own life'))
